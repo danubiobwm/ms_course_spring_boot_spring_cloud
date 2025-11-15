f8065331b140efa0ca37fa02fddfa9ec59a98a8e
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-11-15 10:12:46 -0300

    hr-oauth

---
+++ hr-oauth/src/main/java/com/dev/hrouth/HrOuthApplication.java
@@ -0,0 +1,13 @@
+package com.dev.hrouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOuthApplication.class, args);
+	}
+
+}
---
+++ hr-oauth/src/test/java/com/dev/hrouth/HrOuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.hrouth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
